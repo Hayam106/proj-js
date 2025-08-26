6a49adf02409ea7cdc810019787282a4c86622d6
Hayam106 <Hayam106>
2025-08-26 07:01:17 +0300

    fixed user page

--- js/user.js
+++ js/user.js
@@ -1,44 +1,4 @@
-// let userInfo = document.querySelector ("#n-user")
-// let userD = document.querySelector ("#user")
-// let link = document.querySelector ("#link")
 
-// if (localStorage.getItem("FirstName")){
-//     link.remove()
-//     userInfo.style.display ="flex"
-// userD.innerHTML = "Hello, " + localStorage.getItem("FirstName");
-// }
-// let logOutBtn = document.querySelector("#logout")
-// logOutBtn.addEventListener("click", function (){
-//     localStorage.clear();
-//     setTimeout(() => {
-//         window.location = "login.html";
-//     } , 1000)
-// })
-
-////////////////////////////////////////////////////////////////////////////////////////
-// document.addEventListener("DOMContentLoaded", () => {
-
-//   const userInfo = document.querySelector("#n-user");
-//   const userD = document.querySelector("#user");
-//   const link = document.querySelector("#link");
-//   const logOutBtn = document.querySelector("#logout");
-
-//   if(localStorage.getItem("FirstName")){
-//     if(link) link.remove();
-//     if(userInfo) userInfo.style.display = "flex";
-//     if(userD) userD.innerHTML = "Hello, " + localStorage.getItem("FirstName");
-//   }
-
-//   if(logOutBtn){
-//     logOutBtn.addEventListener("click", function(){
-//       localStorage.clear();
-//       setTimeout(() => {
-//         window.location = "login.html";
-//       }, 1000);
-//     });
-//   }
-
-// });
 
 document.addEventListener("DOMContentLoaded", () => {
 
